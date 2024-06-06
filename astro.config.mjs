7108204ec87a9ae413120de16969ee8977ec1148
rgiova <rgiova.cs@gmail.com>
2024-06-06 19:11:34 -0300

    add deploy config + deploy file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astronaut.github.io',
-    base: 'my-repo'
+    site: 'https://renzogiova.github.io',
+    base: 'astro-blog'
 });
